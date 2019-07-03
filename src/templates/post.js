28f4d53e18ef5a5bc0a8bab7ceacd3a766214cb2
Bryam Rodriguez <brodriguezaybar@gmail.com>
2019-07-03 16:00:53 -0500

    Add renderer for mdx posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-mdx";
 import Layout from "../components/layout";
 
-const PostTemplate = () => {
+export const query = graphql`
+  query($slug: String!) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        author
+      }
+      code {
+        body
+      }
+    }
+  }
+`;
+
+const PostTemplate = ({ data: { mdx: post } }) => {
   return (
     <Layout>
-      <h1>Test</h1>
-      <div dangerouslySetInnerHTML={{ __html: "" }} />
+      <h1>{post.frontmatter.title}</h1>
+      <MDXRenderer>{post.code.body}</MDXRenderer>
     </Layout>
   );
 };
